d1f7741e0b9b9a8bdfcdb325bfbd87de1a32192b
guigui <guilherme.gasp@hotmail.com>
2021-04-24 01:34:49 -0300

    terceiro commit, todos os 7 obj foram add

--- Meshes.js
+++ Meshes.js
@@ -1,10 +1,11 @@
 class Meshes {
-    constructor(url,sceneFile, pos, rot, scale) {
+    constructor(url,sceneFile, pos, rot, scale, meshNum, mat) {
         BABYLON.SceneLoader.ImportMeshAsync("", url, sceneFile).then((result) => {
-            let meshes = result.meshes[0];       
+            let meshes = result.meshes[meshNum];       
             meshes.position = pos;
             meshes.rotation = rot;
             meshes.scaling = scale;
+            meshes.material = mat;
         });
     }
 }
